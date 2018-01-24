efff2f3da45e82950c82e06cd440e4b7f4326a29
Kshitij G <kshitijgorde@rocketmail.com>
2018-01-23 19:45:59 -0500

    Minor bug fix in Delete Comment

--- Project_Blog/blog_app/views.py
+++ Project_Blog/blog_app/views.py
@@ -80,7 +80,7 @@ def comment_approve(request, pk):
 
 @login_required
 def comment_remove(request, pk):
-    comment = get_object_or_404(Comment, pk)
+    comment = get_object_or_404(Comment, pk=pk)
     post_pk = comment.post.pk
 
     comment.delete()
